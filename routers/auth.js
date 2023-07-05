24bd937a8afb39cc3db092da6d39ae472880f73f
Madhan235 <msouljar@gmail.com>
2023-07-05 15:16:41 +0530

    token

--- routers/auth.js
+++ routers/auth.js
@@ -5,10 +5,6 @@ const token = req.headers["auth-token"]
 if(!token){
      return res.status(400).json({data:{error:"Invalid Authorization"}})
     }
-const verfiedToken = jwt.verify(token,process.env.secretkey)
-if(!verfiedToken){ 
-    return  res.status(400).json({data:
-        {verfiedToken:verfiedToken,error:"Token not match"}})
-    }
+jwt.verify(token,process.env.secretkey)
         next();
 }
