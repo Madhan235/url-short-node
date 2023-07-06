76ae4a5f73d95396890b5726e634375be2561d82
Madhan235 <msouljar@gmail.com>
2023-07-06 12:43:07 +0530

    m

--- routers/users.js
+++ routers/users.js
@@ -55,13 +55,10 @@ res.status(200).json({data:{user:user,token:token}})
 router.post("/forget",async function(req,res){
     const {email} = req.body;
 const user = await findUser(email)
-res.send(user)
-console.log(user)
 if(!user){
-    
     return res.status(404).json({data:{error:"email not registered"}})
 }
-const token =  generateForgetToken(user._id) + user.password;
+const token =  generateForgetToken(user._id,user.password);
 const link = `https://madhan235-url-short-node.onrender.com/users/reset/${user._id}/${token}`
 
 let transporter = nodemailer.createTransport({
