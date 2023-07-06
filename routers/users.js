7a6e5ac92d16fc26cb58dc2708d64e624a0ee101
Madhan235 <msouljar@gmail.com>
2023-07-06 12:24:28 +0530

    main

--- routers/users.js
+++ routers/users.js
@@ -55,7 +55,7 @@ res.status(200).json({data:{user:user,token:token}})
 router.post("/forget",async function(req,res){
     const {email} = req.body;
 const user = findUser(email)
-res.send(user)
+res.send(email)
 if(!user){
     
     return res.status(404).json({data:{error:"email not registered"}})
