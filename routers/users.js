d88f8bbbec633e4e4bfb4fae9deb0cae8d060e94
Madhan235 <msouljar@gmail.com>
2023-07-11 11:33:07 +0530

    t

--- routers/users.js
+++ routers/users.js
@@ -135,7 +135,7 @@ router.post("/shortner",async (req,res)=>{
         }
        const shorturl =  shortid.generate(url)
 
-        res.status(200).json({data:{url,shorturl}});
+        res.status(200).json({data:{shorturl}});
 
     
     } catch (error) {
